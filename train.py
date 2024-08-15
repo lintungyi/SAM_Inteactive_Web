e426c8105bfcda0b08294a351c9229a1fc94c38c
ChengHungHsiang <168629269+ChengHungHsiang@users.noreply.github.com>
2024-08-15 14:05:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import warnings
 import torch
 #import monai
 from tqdm import tqdm
+import cv2 as cv
 from statistics import mean
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
@@ -24,13 +25,14 @@ import yaml
 import torch.nn.functional as F
 import losses
 from scribbleprompt import ScribblePromptSAM
+from scribbleprompt import LineScribble
+import gc
+"""
+scribble focused training
+unfinished:batch size can't greater than 1。memory allocation need more optimize
 
 """
-unfinished
-
-"""
-
-class MedScribble:
+class MedScribble(Dataset):
     def __init__(self, path):
         
         if isinstance(path, str):
@@ -63,35 +65,70 @@ class MedScribble:
             seg = np.zeros((256,256))
         
         manual_scribble = np.load(folder / f"scribble_{annotator_idx}.npy")
-        
+        img = torch.from_numpy(img)
+        seg = torch.from_numpy(seg)
+        manual_scribble = torch.from_numpy(manual_scribble)
+        img = img.unsqueeze(0)
+        seg = seg.unsqueeze(0)
         return img, seg, manual_scribble
-        
+            
+    """
+class trainDataset(Dataset):
+    def __init__(self,path,max_pixel):
+        if isinstance(path, str):
+            path = pathlib.Path(path)
+        self.path = path
+        self.item=[]
+        folders = sorted([x.parent for x in self.path.glob("*/*/*/*/*/mask_filname.txt")])
+        for folder in folders:
+            f=open(folder/"mask_filename.txt",'r')
+            f=f.read()
+            name_list=eval(f)
+            mask2scribble=LineScribble(max_pixels=max_pixel)
+            for n in name_list:
+                if (folder / n).exists():
+                    img = cv.imread(folder / n,cv.IMREAD_GRAYSCALE)
+                else:
+                    warnings.warn(f"img missing for {n}.")
+                    img = np.zeros((256,256))
+                if (folder / n).exists():
+                    seg = cv.imread(folder / n,cv.IMREAD_GRAYSCALE)
+                else:
+                    warnings.warn(f"mask missing for {n}.")
+                    seg = np.zeros((256,256))
+                img = img.unsqueeze(0)#1*H*W
+                seg = seg.unsqueeze(0)#1*H*W
+                seg_t= seg.unsqueeze(0)#1*1*H*W
+                img=torch.from_numpy(img)
+                seg=torch.from_numpy(img)
+                img=torch.where(img==64,torch.tensor(1.),torch.tensor(0.))
+                seg=torch.where(seg==64,torch.tensor(1.),torch.tensor(0.))
+                #no neg scribble now
+                pos_scr=mask2scribble.batch_scribble(mask=seg_t)#1*1*H*W
+                scribble=torch.stack([pos_scr[0][0],torch.zeros(pos_scr[0][0].shape())],dim=0)#2*H*W
+                self.item.append((img,seg,scribble))
+        #assert len(self.folders)==5
+
+    def __getitem__(self, index):
+        img,seg,scribble=self.item[index]              
+        return img, seg, scribble
+
+    def __len__(self):
+        return len(self.item)
+ """        
 # Load the config file
 with open("./config.yaml", "r") as ymlfile:
    config_file = yaml.load(ymlfile, Loader=yaml.Loader)
 
-# Take dataset path
-#train_dataset_path = config_file["DATASET"]["TRAIN_PATH"]
-# Load SAM model
-#sam = build_sam_vit_b(checkpoint=config_file["SAM"]["CHECKPOINT"])
-#Create SAM LoRA
-#sam_lora = LoRA_sam(sam, config_file["SAM"]["RANK"])  
-#model = sam_lora.sam
-
 SAM = ScribblePromptSAM()
 model=SAM.model
-
-# Process the dataset
-#processor = Samprocessor(model)
-#train_ds = DatasetSegmentation(config_file, processor, mode="train")
-
-#batch_size=config_file["TRAIN"]["BATCH_SIZE"]
-
-
-# Create a dataloader
-#train_dataloader = DataLoader(train_ds, batch_size=config_file["TRAIN"]["BATCH_SIZE"], shuffle=True, collate_fn=collate_fn)
+max_pixel=config_file["TRAIN"]["MAX_PIXEL"]
+train_dataset=MedScribble(config_file["DATASET"]["TRAIN_PATH"])
+dataloader=DataLoader(dataset=train_dataset,batch_size=config_file["TRAIN"]["BATCH_SIZE"],shuffle=True)#, collate_fn=collate_fn
+print("data loaded")
 # Initialize optimize and Loss
-optimizer = Adam(model.image_encoder.parameters(), lr=1e-4, weight_decay=0)
+optimizer = Adam([{'params': model.image_encoder.parameters()},{'params': model.mask_decoder.parameters()}], lr=1e-4, weight_decay=0)
+#optimizer = Adam(model.image_encoder.parameters(), lr=1e-4, weight_decay=0)
 seg_loss = losses.SAM_loss #monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
 num_epochs = config_file["TRAIN"]["NUM_EPOCHS"]
 
@@ -101,72 +138,57 @@ model.train()
 model.to(device)
 
 total_loss = []
-x = MedScribble("./MedScribble")
 print("start training")
+
+num_step=config_file["TRAIN"]["NUM_STEP"]
 for epoch in range(num_epochs):
     epoch_losses = []
-
-
+    gc.collect()
     #nparray to tensor
-    for i in tqdm(range(len(x.folders))):
-            img, seg, manual_scribble = x[i]
-            img = torch.from_numpy(img)
-            seg = torch.from_numpy(seg)
-            manual_scribble = torch.from_numpy(manual_scribble)
-            #img.to(device)
-            #seg.to(device)
-            #manual_scribble.to(device)
-            img = img.unsqueeze(0)
-            img = img.unsqueeze(0)
-            seg = seg.unsqueeze(0)
-            seg = seg.unsqueeze(0)
-            manual_scribble = manual_scribble.unsqueeze(0)
-
-
+    for img,seg,initial_scribble in tqdm(dataloader):
             """
             mask, img_features, low_res_logits = sp_sam.predict(
             image,        # (B, 1, H, W) 
             scribbles,    # (B, 2, H, W)
             mask_input,   # (B, 1, 256, 256)
             ) # -> (B, 1, H, W), (B, 16, 256, 256), (B, 1, 256, 256)
+            """
+            gc.collect()
+            epoch_losses_t = []
+            global low_res_logits_d
+            threshold_mask=torch.nn.Threshold(0, 0)
+            mask2scribble=LineScribble(max_pixels=max_pixel)
             
-            """
-           
-            mask, img_features, low_res_logits = SAM.predict(img=img.to(device),scribbles=manual_scribble.to(device))
-            loss = seg_loss(low_res_logits, seg.float().to(device))
-            optimizer.zero_grad()
-            loss.backward()
-            # optimize
-            optimizer.step()
-            epoch_losses.append(loss.item())
-
-            """
-            global low_res_logits_t
-            for o in range(2):
-                
+            for o in range(num_step):                
                 if(o==0):
-                    mask, img_features, low_res_logits = SAM.predict(img=img.to(device),scribbles=manual_scribble.to(device))
+                    optimizer.zero_grad()
+                    mask, img_features, low_res_logits = SAM.predict(img=img.to(device),scribbles=initial_scribble.to(device))
                 else:
-                    mask, img_features, low_res_logits = SAM.predict(img=img.to(device),scribbles=manual_scribble.to(device),mask_input=low_res_logits_t.to(device))
+                    error_region=seg.to(device)-mask
+                    false_pos=threshold_mask(error_region*-1)
+                    false_neg=threshold_mask(error_region)
+                    scribbles_t=torch.cat([mask2scribble.batch_scribble(mask=false_neg),mask2scribble.batch_scribble(mask=false_pos)],axis=1)
+                    del error_region,false_pos,false_neg
+                    optimizer.zero_grad()
+                    mask, img_features, low_res_logits = SAM.predict(img=img.to(device),scribbles=scribbles_t.to(device),mask_input=low_res_logits_d.to(device))
                 loss = seg_loss(low_res_logits, seg.float().to(device))
-                optimizer.zero_grad()
-                low_res_logits_t=low_res_logits.clone()
+                gc.collect()
                 loss.backward()
                 # optimize
+                low_res_logits_d=low_res_logits.detach()
                 optimizer.step()
-                epoch_losses.append(loss.item())
-            """
-            
-           
-            
+                optimizer.zero_grad()
+                epoch_losses_t.append(loss.item())
+                del loss, low_res_logits
+                gc.collect()
+            #print(mean(epoch_losses_t))
+            epoch_losses.append(mean(epoch_losses_t))
+            del epoch_losses_t
+            gc.collect()
     print(f'EPOCH: {epoch}')
     print(f'Mean loss training: {mean(epoch_losses)}')
 
 # Save the parameters of the model in safetensors format
 rank = config_file["SAM"]["RANK"]
 SAM.sam_lora.save_lora_parameters(f"lora_rank{rank}.safetensors")
-
-
-
-
-
+torch.save(model.state_dict(),"./scribble_SAM_Lora.pth")
